04e38814a6012b97dad0081d90360582420a29a7
FloiceNyota98 <nyotafloice@gmail.com>
2021-06-15 16:07:09 +0300

    create components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './components/about/about.component';
+import { GithubAppComponent } from './components/github-app/github-app.component';
+import { GithubProfileComponent } from './components/github-profile/github-profile.component';
+import { GithubProfileCardComponent } from './components/github-profile-card/github-profile-card.component';
+import { GithubProfileDataComponent } from './components/github-profile-data/github-profile-data.component';
+import { GithubReposComponent } from './components/github-repos/github-repos.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    GithubAppComponent,
+    GithubProfileComponent,
+    GithubProfileCardComponent,
+    GithubProfileDataComponent,
+    GithubReposComponent,
+    
   ],
   imports: [
     BrowserModule,
